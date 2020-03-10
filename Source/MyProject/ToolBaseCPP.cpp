e094c80e009d85cdec5afc58868cce36141a2cbe
asd <developer@daniel-fowke.tech>
2020-03-10 17:14:55 +0000

    Initialisation of Prototype Redevelopment
    The task here is to redevelop all core components from the ground up, fixing any main issues and future proofing the development process best as possible for now. This will look at overall performance improvements and development of functions to further make the development of the game more consistent

---
+++ Source/MyProject/ToolBaseCPP.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ToolBaseCPP.h"
+
+// Sets default values
+AToolBaseCPP::AToolBaseCPP()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AToolBaseCPP::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AToolBaseCPP::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
