5a8cd5995626762702a276bb424c7abf80a24fc8
NGDev <developer@daniel-fowke.tech>
2020-05-07 16:54:36 +0100

    Rebuild Base in progress for pre-alpha

---
+++ Source/SuperWildFireFighter/SuperWildFireFighterGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SuperWildFireFighterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERWILDFIREFIGHTER_API ASuperWildFireFighterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
